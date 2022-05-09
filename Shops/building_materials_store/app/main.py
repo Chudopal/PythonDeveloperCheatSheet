23f8d6c26ea978340ee75e1794520cea2fc48142
Nikita5390 <nikitakashevskii@mail.ru>
2022-05-09 22:13:23 +0300

    update SHOP

--- Shops/building_materials_store/app/main.py
+++ Shops/building_materials_store/app/main.py
@@ -1,6 +1,4 @@
-"""
-Кашевский Никита
-5. Напишите программу магазина.
+"""5. Напишите программу магазина.
 Покупатели могут покупать какие-то продукты в магазине.
 Возможности покупателя:
 - посмотреть все товары и цены на них
@@ -10,32 +8,75 @@
 Товары храните просто в какой-нибудь из коллекций.
 Выбор товара - это ввод пользователем строки названия товара
 """
-print('Hello World')
-SHOP = {
-    'apple': 5,
-    'orange' : 7,
-    'tomato' : 4,
-    'potato' : 2,
-}
-buy_product = []
+
+# SHOP = {
+#     'apple': 5,
+#     'orange' : 7,
+#     'tomato' : 4,
+#     'potato' : 2,
+# }
+# buy_product = []
+
+import json
 from typing import List, Dict
 
-def get_all_product() -> List:
-    return SHOP
 
-def add_product(product_name: str) -> None:
-    buy_product.append(SHOP.get(product_name))
+def read_file(path, name) -> List:    #чтение json файла
+    with open(path) as file:
+        data = json.load(file)
+    return data.get(name)
 
-def get_buy_product() -> List:
+def write_file(path, data) -> None:  #запись json файла
+    with open(path, 'w') as file:
+        json.dump(data, file)
+
+
+def adaptor(data: List) -> Dict:  #конвертация листа в словарь
+    result = {}
+    for materials in data:
+        result[materials.get('material_name')] = materials.get('cost')
+    return result
+
+
+def save_adaptor(data: Dict) -> List: # конвертация словаря в лист
+    result = []
+    for material_name, material_cost in data.items():
+        result.append(
+            {"material_name": material_name,
+             "cost": material_cost}
+        )
+    return result
+
+
+
+def get_all_product() -> List:    #показывает все материалы магазина
+    data = read_file('storage_building.json', 'materials')
+    return adaptor(data)
+
+
+def get_buy_materials() -> List:    #показывает выбранные в корзину материалы
+    data = read_file('buy_materials.json', 'materials')
+    return adaptor(data)
+
+
+def add_product(product_name: str) -> None:    #добавляет товар в корзину!!! NO WORK
+    # buy_product.append(SHOP.get(product_name))
+    save_adaptor()
+    write_file('buy_materials.json', save_adaptor(get_all_product()))
+
+def get_buy_product() -> List:        #показывает сумму покупки!!! NO WORK сделается после add_product
     total_price = sum(buy_product)
     result = (f'Ваша сумма покупки составляет {total_price} руб')
     return result
+
 
 def format_product(product_list: Dict) -> str:
     return '\n'.join([
         f'{product_name} - {cost} руб'
         for product_name, cost in product_list.items()
     ])
+
+
 def menu() -> str:
     return (
     '<>'*80 + '\n'
@@ -45,7 +86,8 @@ def menu() -> str:
     '4 - завершить покупку.'
     )
 
-def make_choice(choice: int):
+
+def make_choice(choice: int):                #пока вообще не трогал
     if choice == 1:
         product = get_all_product()
         message = format_product(product)
