3cff02daf180b4cca94999bb9a7ab73cbf9f10dc
unknown <ilyalikhodeev@mail.ru>
2022-05-10 18:40:09 +0300

    refact1

--- Shops/car_shop/app/main.py
+++ Shops/car_shop/app/main.py
@@ -1,18 +1,21 @@
 import json
 
-
-with open('my_cars.json', 'r') as f:
-    my_cars = json.load(f)
-    
-
-with open('bag.json', 'r') as f:
-    bag = json.load(f)
+def get_cars_from_file():
+    with open('my_cars.json', 'r') as f:
+        my_cars = json.load(f)
+    return my_cars
+        
+        
+def get_cars_bag():
+    with open('bag.json', 'r') as f:
+        bag = json.load(f)
+    return bag
 
 
 def choice_cars():
     while True:
         count = 0
-        for i in my_cars['cars']:
+        for i in get_cars_from_file()['cars']:
             count += 1
             tamplate = f'{count}. model: {i["model"]}, price: ${i["price"]}'
             print(tamplate)
@@ -24,22 +27,23 @@ def choice_cars():
 
 def add_to_bag():
     choice = int(input('Выберите номер авто:'))
-    if choice != 0:     
-        bag['cars'].append(my_cars['cars'][choice - 1])
-        save_bag()
+    if choice != 0:
+        cars = get_cars_bag()
+        cars['cars'].append(get_cars_from_file()['cars'][choice - 1])
+        save_bag(cars)
     return choice
 
 
-def save_bag():
+def save_bag(cars):
     with open('bag.json', 'w') as f:
-        json.dump(bag, f)
+        json.dump(cars, f)
 
 
 def show_bag():
     while True:
         total = 0
         count = 0
-        for i in bag['cars']:
+        for i in get_cars_bag()['cars']:
             count += 1
             total += i["price"]
             tamplate = f'{count}. model: {i["model"]}, price: ${i["price"]}'
@@ -56,13 +60,13 @@ def show_bag():
 def del_bag():
     choice = int(input('Выберите авто для удаления: '))
     if choice != 0:
-        del bag['cars'][choice - 1]
+        del get_cars_bag()['cars'][choice - 1]
         print('Авто успешно удалено!')
         save_bag()
     return choice
 
 
-def mine_menu():
+def main_menu():
     while True:
         print("""
         0 - Выход
@@ -79,4 +83,4 @@ def mine_menu():
         else:
             print('Неверный выбор')
 
-mine_menu()
+main_menu()
