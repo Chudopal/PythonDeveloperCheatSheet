bc28c13d40dd62e6b9fe898689adc6ffe473066c
Denis1505 <victori_l98@mail.ru>
2022-05-11 14:52:56 +0300

    Some Logic

--- Shops/sport_atribute_shop/app/main.py
+++ Shops/sport_atribute_shop/app/main.py
@@ -13,17 +13,17 @@ from typing import List
 
 
 PRODUCT = [
-    ("Мяч", 100),
-    ("Бутсы", 200),
-    ("Гетры", 15),
-    ("Шорты", 60),
-    ("Майка", 70), 
-    ("Щетки", 25)
+        ("Мяч", 100),
+        ("Бутсы", 200),
+        ("Гетры", 15),
+        ("Шорты", 60),
+        ("Майка", 70), 
+        ("Щетки", 25)
 ]
 
 
 def format_product(product_list: any) -> str:
-    return
+    return ([f"Товар {product[0]} стоит {product[1]}" for product in product_list])
 
 def menu() -> str:
     return ("1. Посмотреть все товары и цены на них\n" +
@@ -31,7 +31,8 @@ def menu() -> str:
     "3. Посмотреть сумму покупки.")
 
 def look() -> List:
-    ...
+    return PRODUCT
+    
 
 def choices():
     ...
@@ -45,13 +46,15 @@ def run() -> None:
     while True:
         choice = int(input("Выберите пункт меню: "))
         if choice == 1:
-            look()
+            product = look()
+            massage = format_product(product)
+            print(massage)
         elif choice == 2:
             product = int(input("Выберите товар: "))
             choices(product)
         elif choice == 3:
             perchoice()
         else:
-            print("Не корректный ввод. Поробуйте снова")
+            print("Не корректный ввод. Попробуйте снова")
 
 run()
