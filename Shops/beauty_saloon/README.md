0a8697113d6cd025df7195e15474e5adc2b25520
Pozitiv4ik <z70.python@gmail.com>
2022-05-11 22:28:56 +0300

    new funcy

--- Shops/beauty_saloon/README.md
+++ Shops/beauty_saloon/README.md
@@ -1,2 +1,3 @@
 файлы хранилища:
 list_services.json - список услуг салона
+services_basket.json - список услуг в корзине
--- Shops/beauty_saloon/app/main.py
+++ Shops/beauty_saloon/app/main.py
@@ -2,10 +2,14 @@
 Возможности:
 - просмотреть все услуги
 - добавить услугу в корзину
+- посмотреть сумму выбранных услуг
 """
 
 import json
 
+# знаю что комменты нельзя. но мы же учимся:)
+
+# функция которая показывает список услуг 
 
 def list_of_services():
     with open("app\list_services.json", encoding='utf-8') as file:
@@ -14,5 +18,50 @@ def list_of_services():
         for i in result["list_of_services"]:
             r += (f"{i['service_number']}. {i['name_of_services']} - {i['price_of_service']}р.\n")
     return r
-    
+
+# функция которая показывает выбранные услуги и их стоимость
+
+def basket_of_services():
+    with open("app\services_basket.json", encoding='utf-8') as file:
+        result  = json.load(file)
+        r = ''
+        counter = 0
+        for i in result["list_of_services"]:
+            counter += i['price_of_service']
+            r += (f"{i['service_number']}. {i['name_of_services']} - {i['price_of_service']}р.\n")
+    return r + f'Итого: {counter}'
+
+
+# функция выбора услуг 
+
+def adding_service():
+    ...
+
+
+# функция навигатор по меню
+
+def make_main_choice(choice):
+    result = ""
+    if choice == 1:
+        result = list_of_services()
+    elif choice == 2:
+        result = adding_service()
+    elif choice == 3:
+        result = basket_of_services()
+    elif choice != 4:
+        result = 'Неверный выбор'
+    return result
+
+
+
+# функция первого собщения меню
+
+def menu():
+    message = """
+        1 - Посмотреть услуги
+        2 - Выбор услуг
+        3 - Посмотреть список выбранных услуг
+        4 - Выход
+    """ + "\n" + "Выберите действие: "
+    return int(input(message))
 
