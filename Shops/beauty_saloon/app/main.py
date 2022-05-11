ee54e685b7b001bc7ba30b3c58226748f7d481bd
Pozitiv4ik <z70.python@gmail.com>
2022-05-11 19:34:31 +0300

    created a service list function

--- Shops/beauty_saloon/app/main.py
+++ Shops/beauty_saloon/app/main.py
@@ -1,71 +1,18 @@
-print("Hello world")
-BAG = []
-CARS = [
-    ('Merc', 100),
-    ('BMW', 1000000),
-    ('Audi', 50),
-    ('VW', 20),
-    ('Ford', 10)
-]
+"""Программа для салона красоты (Лэшмейкера и бровиста).
+Возможности:
+- просмотреть все услуги
+- добавить услугу в корзину
+"""
+
+import json
 
 
-def format_products(cars) -> str:
-    id = 0
-    result = '0 - Выход\n'
-    for i in cars:
-        id += 1
-        result += f'{id}.{i[0]} - ${i[1]}' + "\n"
-    return result + "Выберите товар: "
+def list_of_services():
+    with open("app\list_services.json", encoding='utf-8') as file:
+        result  = json.load(file)
+        r = ''
+        for i in result["list_of_services"]:
+            r += (f"{i['service_number']}. {i['name_of_services']} - {i['price_of_service']}р.\n")
+    return r
+    
 
-
-def show_BAG():
-    total = 0
-    result = ""
-    for i in BAG:
-        result += i[0] + '\n'
-        total += i[1]
-
-    result = result + f'{total}$ - Общая сумма покупок' + "\n"
-    return result
-
-
-def make_main_choice(choice):
-    result = ""
-    if choice == 1:
-        result = add_products(CARS)
-    elif choice == 2:
-        result = show_BAG()
-    elif choice != 0:
-        result = 'Неверный выбор'
-    return result
-
-
-def add_products(cars):
-    message = format_products(cars)
-    result = launch(add_product, input_message=message)
-    return result
-
-
-def add_product(choice):
-    BAG.append(CARS[choice - 1])
-    return 'Товар успешно добавлен в корзину.'
-
-
-def launch(func, input_message):
-    choice = None
-    while choice != 0:
-        choice = int(input(input_message))
-        message = func(choice)
-        print(message)
-
-
-def run():
-    message = """
-        0 - Выход
-        1 - Посмотреть товары/Добавить в корзину
-        2 - Посмотреть корзину
-    """ + "\n" + "Выберите действие: "
-    launch(make_main_choice, input_message = message)
-
-
-run()
