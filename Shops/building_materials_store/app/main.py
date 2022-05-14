daf968fe3f300872de27da1b10dd1b2a619ab2b2
Nikita5390 <nikitakashevskii@mail.ru>
2022-05-15 01:48:52 +0300

    code is work

--- Shops/building_materials_store/app/main.py
+++ Shops/building_materials_store/app/main.py
@@ -9,14 +9,6 @@
 Выбор товара - это ввод пользователем строки названия товара
 """
 
-# SHOP = {
-#     'apple': 5,
-#     'orange' : 7,
-#     'tomato' : 4,
-#     'potato' : 2,
-# }
-# buy_product = []
-
 import json
 from typing import List, Dict
 
@@ -25,6 +17,13 @@ def read_file(path, name) -> List:    #чтение json файла
     with open(path) as file:
         data = json.load(file)
     return data.get(name)
+
+
+def read_buy_file(path) -> List:    #чтение json файла
+    with open(path) as file:
+        data = json.load(file)
+    return data
+
 
 def write_file(path, data) -> None:  #запись json файла
     with open(path, 'w') as file:
@@ -38,34 +37,39 @@ def adaptor(data: List) -> Dict:  #конвертация листа в слов
     return result
 
 
-def save_adaptor(data: Dict) -> List: # конвертация словаря в лист
-    result = []
-    for material_name, material_cost in data.items():
-        result.append(
-            {"material_name": material_name,
-             "cost": material_cost}
-        )
-    return result
-
-
-
 def get_all_product() -> List:    #показывает все материалы магазина
     data = read_file('storage_building.json', 'materials')
-    return adaptor(data)
+    return data
 
 
 def get_buy_materials() -> List:    #показывает выбранные в корзину материалы
-    data = read_file('buy_materials.json', 'materials')
-    return adaptor(data)
+    data = read_buy_file('buy_materials.json')
+    return data
+print(get_buy_materials())
 
 
-def add_product(product_name: str) -> None:    #добавляет товар в корзину!!! NO WORK
-    # buy_product.append(SHOP.get(product_name))
-    save_adaptor()
-    write_file('buy_materials.json', save_adaptor(get_all_product()))
+buy_product = []
+def save_buy_product(buy_product):
+    data = write_file('buy_materials.json', buy_product)
 
-def get_buy_product() -> List:        #показывает сумму покупки!!! NO WORK сделается после add_product
-    total_price = sum(buy_product)
+
+def add_product(product_name: str) -> None:    #добавляет товар в корзину
+    catalog = get_all_product()
+    for i in catalog:
+        item_cost = i['cost']
+        item_material = i['material_name']
+        if product_name == item_material:
+            result = dict.fromkeys([item_material], item_cost)
+            buy_product.append(result)
+
+
+def get_buy_product() -> List:        #показывает сумму покупки
+    result_list = []
+    for i in buy_product:
+        result = list(i.values())
+        for i in result:
+            result_list.append(i)
+    total_price = sum(result_list)
     result = (f'Ваша сумма покупки составляет {total_price} руб')
     return result
 
@@ -82,28 +86,30 @@ def menu() -> str:
     '<>'*80 + '\n'
     '1 - посмотреть все товары и цены на них\n' +
     '2 - выбрать товар\n' +
-    '3 - посмотреть сумму покупки(сумма цен выбранных товаров)\n' +
-    '4 - завершить покупку.'
+    '3 - сохранить выбранные товары в корзине\n' +
+    '4 - посмотреть сумму покупки(сумма цен выбранных товаров)\n' +
+    '5 - завершить покупку.'
     )
 
 
-def make_choice(choice: int):                #пока вообще не трогал
+def make_choice(choice: int):
     if choice == 1:
-        product = get_all_product()
+        product = adaptor(get_all_product())
         message = format_product(product)
         result = message
     elif choice == 2:
         product_name = input('Введите желаемый товар: ')
         result = add_product(product_name)
-    elif choice == 3:
+    elif choice ==3:
+        result = save_buy_product(buy_product)
+    elif choice == 4:
         result = get_buy_product()
-
     return result
 
 
 def run() -> None:
     choice = None
-    while choice != 4:
+    while choice != 5:
         print(menu())
         choice = int(input('Введите пункт меню: '))
         message = make_choice(choice)
