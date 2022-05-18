1a2993177dc9a6355556023fd6e22d8891fd108b
Xejb <vladiq-artamonenko@rambler.ru>
2022-05-18 21:37:20 +0300

    class shop

--- Shops/hardware_store/app/main.py
+++ Shops/hardware_store/app/main.py
@@ -1,46 +1,60 @@
 import json
 from typing import List, Dict
 
+
 PATH_JSON = '../shop_invent.json'
 WRITE_JSON = '../shopping_card.json'
 
-BUY_PRODUCTS = []
 
-SHOP = {}
+class JsonFileHandler:
 
+    def __init__(self, path: str):
+        self.path = path
 
-def get_all_product() -> Dict:
-    if not SHOP:
-        SHOP.update(read_file())
-    return SHOP
+    def write_file(self, data) -> None:
+        with open(self.path, 'w') as file:
+            json.dump(data, file)
 
-
-def read_file() -> List:
-    with open(PATH_JSON, 'r', encoding='utf8') as file:
-        return json.load(file)
-
-
-def write_file(path, data) -> None:
-    with open(path, 'w') as file:
-        json.dump(data, file)
+    def read_file(self) -> List:
+        with open(self.path, 'r', encoding='utf8') as file:
+            return json.load(file)
 
 
-def add_product(product_name: str) -> None:
-    BUY_PRODUCTS.append(get_all_product().get(product_name))
-    write_file(WRITE_JSON, BUY_PRODUCTS)
+class ActionStore:
 
+    BUY_PRODUCTS = []
+    SHOP = {}
 
-def get_buy_product() -> List:
-    total_price = sum(BUY_PRODUCTS)
-    result = (f'Your purchase amount is {total_price} dollars')
-    return result
+    shop_invent = JsonFileHandler(PATH_JSON)
+    shopping_card = JsonFileHandler(WRITE_JSON)
 
+    def get_all_product(self) -> Dict:
+        if not self.SHOP:
+            self.SHOP.update(self.shop_invent.read_file())
+        return self.SHOP
 
-def format_product(product_list: Dict) -> str:
-    return '\n'.join([
-        f'{product_name} - {cost} dollars'
-        for product_name, cost in product_list.items()
-    ])
+    def add_product(self, product_name: str) -> None:
+        self.BUY_PRODUCTS.append(self.get_all_product().get(product_name))
+        self.shopping_card.write_file(self.BUY_PRODUCTS)
+
+    def get_buy_product(self) -> List:
+        total_price = sum(self.BUY_PRODUCTS)
+        result = (f'Your purchase amount is {total_price} dollars')
+        return result
+
+    def format_product(self, product_list: Dict) -> str:
+        return '\n'.join([
+            f'{product_name} - {cost} dollars'
+            for product_name, cost in product_list.items()
+        ])
+
+    def clear_buy_products(self):
+        self.BUY_PRODUCTS.clear()
+
+    def check_product(self, product_name):
+        return self.get_all_product().get(product_name)
+
+shop = ActionStore()
 
 
 def menu() -> str:
@@ -49,25 +63,34 @@ def menu() -> str:
     '1 - View all products and their prices\n' +
     '2 - select product\n' +
     '3 - view purchase amount\n' +
-    '4 - complete your purchases.'
+    '4 - complete your purchases.\n' +
+    '5 - Close shop'
     )
 
 
 def make_choice(choice: int):
     result = ""
     if choice == 1:
-        products = get_all_product()
-        message = format_product(products)
+        products = shop.get_all_product()
+        message = shop.format_product(products)
         result = message
     elif choice == 2:
         products = input("select product: ")
-        add_product(products)
-        result = 'you have selected a product'
+        if shop.check_product(products) == None:
+            result = 'You have entered a non-existent product, please repeat'
+        else:
+            shop.add_product(products)
+            result = f'you have selected a {products} product'
     elif choice == 3:
-        result = get_buy_product()
+        result = shop.get_buy_product()
     elif choice == 4:
-        result = get_buy_product()
-        BUY_PRODUCTS.clear()
+        result = shop.get_buy_product()
+        shop.clear_buy_products()
+    elif (choice < 1) or (choice > 5):
+        result = 'Enter a number from 1 to 5'
+    elif choice == 5:
+        result = 'Shop is closed, see you soon!'
+
     return result
 
 
@@ -75,8 +98,12 @@ def run() -> None:
     choice = None
     while choice != 5:
         print(menu())
-        choice = int(input('Enter menu item: '))
-        message = make_choice(choice)
-        print(message)
+        try:
+            choice = int(input('Enter menu item: '))
+            message = make_choice(choice)
+            print(message)
+        except ValueError:
+            print("You have entered a string, enter a number!")
+
 
 run()
