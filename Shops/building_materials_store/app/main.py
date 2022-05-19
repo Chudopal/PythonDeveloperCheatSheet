192eced5426dc3fa814090aafb7a07080c58836c
Nikita5390 <nikitakashevskii@mail.ru>
2022-05-19 22:41:25 +0300

    aded new class

--- Shops/building_materials_store/app/main.py
+++ Shops/building_materials_store/app/main.py
@@ -31,48 +31,49 @@ class JsonHandler:
 
     def write_file(path, data) -> None:  # запись json файла
         with open(path, 'w') as file:
-            json.dump(data, file)
+            json.dump(data, file, indent=4)
 
 
-def adaptor(data: List) -> Dict:  # конвертация листа в словарь
-    result = {}
-    for materials in data:
-        result[materials.get('material_name')] = materials.get('cost')
-    return result
+class FileManager:
+    def __init__(self, buy_product):
+        self.buy_product = buy_product
 
+    def get_all_product() -> List:  # показывает все материалы магазина
+        data = JsonHandler.read_file('storage_building.json', 'materials')
+        return data
 
-def get_all_product() -> List:  # показывает все материалы магазина
-    data = JsonHandler.read_file('storage_building.json', 'materials')
-    return data
-
-
-buy_product = []
-
-
-def save_buy_product(buy_product):  # сохраняет добавленный товар в корзине
-    data = JsonHandler.write_file('buy_materials.json', buy_product)
-    return "Товар успешно сохранён в корзину!"
+    def save_buy_product(buy_product):  # сохраняет добавленный товар в корзине
+        data = JsonHandler.write_file('buy_materials.json', buy_product)
+        return "Товар успешно сохранён в корзину!"
 
 
 class ServiceFun:
-    def __init__(self, product_name):
+    def __init__(self, product_name, data, product_list):
         self.product_name = product_name
+        self.data = data
+        self.product_list = product_list
 
+    buy_product = []
+
+    def adaptor(data: List) -> Dict:  # конвертация листа в словарь
+        result = {}
+        for materials in data:
+            result[materials.get('material_name')] = materials.get('cost')
+        return result
 
     def add_product(product_name: str) -> None:  # добавляет товар в корзину
-        catalog = get_all_product()
+        catalog = FileManager.get_all_product()
         for material in catalog:
             item_cost = material['cost']
             item_material = material['material_name']
             if product_name == item_material:
                 result = {item_material: item_cost}
-                buy_product.append(result)
-        return "Спасибо! Товар добавлен в корзину."
-
+                ServiceFun.buy_product.append(result)
+        return "Спасибо! Товар добавлен в корзину!"
 
     def get_buy_product() -> List:  # показывает сумму покупки
         result_list = []
-        for material in buy_product:
+        for material in ServiceFun.buy_product:
             result = list(material.values())
             for material in result:
                 result_list.append(material)
@@ -80,48 +81,53 @@ class ServiceFun:
         result = (f'Ваша сумма покупки составляет {total_price} руб')
         return result
 
-
-def format_product(product_list: Dict) -> str:
-    return '\n'.join([
-        f'{product_name} - {cost} руб'
-        for product_name, cost in product_list.items()
-    ])
-
-
-def menu() -> str:
-    return (
-            '<>' * 80 + '\n'
-                        '1 - посмотреть все товары и цены на них\n' +
-            '2 - выбрать товар\n' +
-            '3 - сохранить выбранные товары в корзине\n' +
-            '4 - посмотреть сумму покупки(сумма цен выбранных товаров)\n' +
-            '5 - завершить покупку.'
-    )
+    def format_product(product_list: Dict) -> str:
+        return '\n'.join([
+            f'{product_name} - {cost} руб'
+            for product_name, cost in product_list.items()
+        ])
 
 
-def make_choice(choice: int):
-    if choice == 1:
-        print('СПИСОК ДОСТУПНЫХ ТОВАРОВ:')
-        product = adaptor(get_all_product())
-        message = format_product(product)
-        result = message
-    elif choice == 2:
-        product_name = input('Введите желаемый товар: ')
-        result = ServiceFun.add_product(product_name)
-    elif choice == 3:
-        result = save_buy_product(buy_product)
-    elif choice == 4:
-        result = ServiceFun.get_buy_product()
-    return result
+class SHOP:
+    def __init__(self, choice):
+        self.choice = choice
+
+    def menu() -> str:
+        return (
+                '<>' * 80 + '\n'
+                            '1 - посмотреть все товары и цены на них\n' +
+                '2 - выбрать товар\n' +
+                '3 - сохранить выбранные товары в корзине\n' +
+                '4 - посмотреть сумму покупки(сумма цен выбранных товаров)\n' +
+                '5 - завершить покупку.'
+        )
+
+    def make_choice(choice: int):
+        if choice == 1:
+            print('СПИСОК ДОСТУПНЫХ ТОВАРОВ:')
+            product = ServiceFun.adaptor(FileManager.get_all_product())
+            message = ServiceFun.format_product(product)
+            result = message
+        elif choice == 2:
+            product_name = input('Введите желаемый товар: ')
+            result = ServiceFun.add_product(product_name)
+        elif choice == 3:
+            result = FileManager.save_buy_product(ServiceFun.buy_product)
+        elif choice == 4:
+            result = ServiceFun.get_buy_product()
+        return result
 
 
 def run() -> None:
     choice = None
     while choice != 5:
-        print(menu())
-        choice = int(input('Введите пункт меню: '))
-        message = make_choice(choice)
-        print(message)
+        print(SHOP.menu())
+        try:
+            choice = int(input('Введите пункт меню: '))
+            message = SHOP.make_choice(choice)
+            print(message)
+        except ValueError:
+            print("Введите целое число от 1 до 5!")
 
 
 run()
