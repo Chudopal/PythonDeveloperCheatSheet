e426a4ca164b4605a0a75a14d07d61855b121154
Eugene Syroezhkin <mail.lpfun@gmail.com>
2022-06-04 17:41:44 +0300

    [#H15].(animal_world hw done)

--- Shops/pizzeria/homeworks/animal_world.py
+++ Shops/pizzeria/homeworks/animal_world.py
@@ -14,3 +14,33 @@
 
 Опишите это в коде.
 """
+
+
+class Animal:
+
+    def __init__(self, name: str, age: int):
+        self.name = name
+        self.age = age
+
+    def presentate(self):
+        print(f'My name is {self.name}, my age is {self.age}')
+
+
+class Dog(Animal):
+
+    def __init__(self, name: str, age: int, fav_command: str):
+        self.fav_command = fav_command
+        super().__init__(name, age)
+
+    def make_sound(self):
+        print(f"Hello, I'm {self.name}, and I'm woofing! My favorite command is {self.fav_command}!")
+
+
+class Cat(Animal):
+
+    def __init__(self, name: str, age: int, fav_place: str):
+        self.fav_place = fav_place
+        super().__init__(name, age)
+
+    def make_sound(self):
+        print(f"Hello, I'm {self.name}, and I'm meowing! My favorite place is {self.fav_place}!")
