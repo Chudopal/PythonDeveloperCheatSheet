9f591086477500edd80ae2033efd82e5de3349a1
unknown <ilyalikhodeev@mail.ru>
2022-06-09 10:42:13 +0300

    [#H15] Added presentate

--- Shops/car_shop/HomeWork/animal.py
+++ Shops/car_shop/HomeWork/animal.py
@@ -5,6 +5,10 @@ class Animal:
         self.name = name
         self.age = age
 
+    
+    def presentate(self):
+        print(f"My name is {self.name}, my age is {self.age}")
+
 
 class Dog(Animal):
 
@@ -23,17 +27,21 @@ class Dog(Animal):
 class Cat(Animal):
 
 
-    def __init__(self, name: str, age: int, favorite_place: str):
-        self.favorite_place = favorite_place
+    def __init__(self, name: str, age: int, fav_place: str):
+        self.fav_place = fav_place
         super().__init__(name, age)
 
 
-    def favorite_place(self):
-        print(f"Hello, I'm {self.name}, and I'm meowing! My favorite place is {self.favorite_place}!")
+    def sit_favorite_place(self):
+        print(f"Hello, I'm {self.name}, and I'm meowing! My favorite place is {self.fav_place}!")
         
 
 cat = Cat('Barsik', 2, 'Table')
-cat.favorite_place()
+cat.sit_favorite_place()
+cat.presentate()
         
 dog = Dog('Sharik', 5, 'Sit')
 dog.make_command()
+dog.presentate()
+
+
