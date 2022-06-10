3dfda99cc9dc2488ad6810a462af4d0a653886a9
Кристина <podkas.kristina@gmail.com>
2022-06-10 11:24:58 +0300

    [#HW15].(polymorphism added)

--- Shops/stationary_store/Homework/HW/Animal.py
+++ Shops/stationary_store/Homework/HW/Animal.py
@@ -7,27 +7,23 @@ class Animal:
     def presentate (self):
         return f'My name is {self.name}, my age is {self.age}'
 
-class Dog:
+class Dog(Animal):
 
-    def __init__ (self, dog: Animal, command: str):
-        self.dog = dog
+    def __init__ (self, name: str, age: int, command: str):
+        self.name = name
+        self.age = age
         self.command = command
 
-    def bark (self):
-        return f"Hello, I'm {dog.name}, and I'm woofing! My favorite command is {self.command}!"
+    def make_sound (self):
+        return f"Hello, I'm {self.name}, and I'm woofing! My favorite command is {self.command}!"
 
 
-class Cat:
+class Cat(Animal):
 
-    def __init__ (self, cat: Animal, fav_place: str):
-        self.cat = cat
+    def __init__ (self, name: str, age: int, fav_place: str):
+        self.name = name
+        self.age = age
         self.fav_place = fav_place
 
-    def mew (self):
-        return f"Hello, I'm {cat.name}, and I'm meowing! My favorite place is {self.fav_place}!"
-
-
-dog = Animal ("Movie", 2)
-cat = Animal ("Molly", 2)
-movie = Dog(dog,"sit")
-molly = Cat(cat,"armchair")
+    def make_sound (self):
+        return f"Hello, I'm {self.name}, and I'm meowing! My favorite place is {self.fav_place}!"
