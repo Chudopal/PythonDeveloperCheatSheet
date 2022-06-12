d7a5884437727528325fdac3cf9ed2e90b969444
Eugene Syroezhkin <mail.lpfun@gmail.com>
2022-06-12 12:28:56 +0300

    [#H17].(Completed 2nd task)

--- Shops/pizzeria/homeworks/flask_hw/2_task.py
+++ Shops/pizzeria/homeworks/flask_hw/2_task.py
@@ -25,23 +25,85 @@ localhost:5000/cars
 (extra) Дабавьте фильтрацию через адресную строку по всем этим параметрам.
 """
 
-import json
 from flask import Flask
 from flask import jsonify
-
+from flask import request
 
 app = Flask(__name__)
 
 
-def get_cars():
-    with open("storage.json") as file:
-        data = json.load(file).get("2_task").get("cars")
-    return data
+class CarsStorage:
+
+    def __init__(self, path):
+        self.path = path
+        self.data = self.read()
+
+    def find_cars(self, **kwargs) -> list:
+        response = self.get_all_cars().get("cars")
+
+        if kwargs.get('car_id'):
+            response = filter(lambda cars: cars.get("id") == kwargs.get('car_id'), response)
+        if kwargs.get("price_usd"):
+            response = filter(lambda cars: cars.get("price_usd") == kwargs.get("price_usd"), response)
+        if kwargs.get("brand"):
+            response = filter(lambda cars: cars.get("brand") == kwargs.get("brand").capitalize(), response)
+        if kwargs.get("model"):
+            response = filter(lambda cars: cars.get("model") == kwargs.get("model").capitalize(), response)
+        if kwargs.get("year"):
+            response = filter(lambda cars: cars.get("year") == kwargs.get("year"), response)
+        if kwargs.get("rain_detector"):
+            response = filter(lambda cars: cars.get("rain_detector") == kwargs.get("rain_detector"), response)
+        if kwargs.get("generation"):
+            response = filter(lambda cars: cars.get("generation") == kwargs.get("generation"), response)
+        if kwargs.get("interior_material"):
+            response = filter(lambda cars: cars.get("interior_material") == kwargs.get("interior_material"), response)
+        if kwargs.get("created_advert"):
+            response = filter(lambda cars: cars.get("created_advert") == kwargs.get("created_advert"), response)
+
+        return list(response)
+
+    def get_car_by_id(self, car_id):
+        return self.find_cars(car_id=str(car_id))
+
+    def get_all_cars(self):
+        result = {"cars_number": len(self.data)}
+        cars = []
+        for car in self.data:
+            car_data = {
+                "id": car.get("id"),
+                f"price_{car.get('price').get('currency')}": car.get("price").get("amount"),
+                "created_advert": car.get("publishedAt")
+            }
+
+            car_data.update(
+                {car_property.get('name'): car_property.get("value") for car_property in car.get('properties')}
+            )
+            cars.append(car_data)
+        result.update({"cars": cars})
+
+        return result
+
+    def save(self):
+        import json
+        with open(self.path, "w") as file:
+            json.dump(file, self.data)
+
+    def read(self):
+        import json
+        with open(self.path) as file:
+            data = json.load(file).get("2_task").get("cars")
+        return data
 
 
-@app.route("/cars")
+@app.route("/cars/", methods=["GET"])
 def get_cars_view():
-    cars = get_cars()
-    return jsonify(cars)
+    get_args = request.args.to_dict()
+    cars = CarsStorage("storage.json")
+    if get_args:
+        result = cars.find_cars(**get_args)
+    else:
+        result = cars.get_all_cars()
+    return jsonify(result)
+
 
 app.run(port=5000)
