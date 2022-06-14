053e647c7464a37c9c7a2eb33c484530bb480481
Eugene Syroezhkin <61776541+mega-yujin@users.noreply.github.com>
2022-06-14 12:15:08 +0300

    [#H17}.(Refactored 3rd task)
    Added dates conversion from str to datetime obj.

--- Shops/pizzeria/homeworks/flask_hw/3_task.py
+++ Shops/pizzeria/homeworks/flask_hw/3_task.py
@@ -16,6 +16,7 @@ localhost:5000/schedule/monday/06-06-2022
 """
 
 import json
+from datetime import datetime
 from flask import Flask
 from flask import render_template
 
@@ -24,10 +25,11 @@ app = Flask(__name__)
 
 class ScheduleStorage:
 
-    def __init__(self, file_path: str, data_path: tuple) -> None:
+    def __init__(self, file_path: str, data_path: tuple, datetime_pattern: str) -> None:
         self.file_path = file_path
         self.data_path = data_path
-        self.data = self._get_data(self._read_file())
+        self.datetime_pattern = datetime_pattern
+        self.data = self._convert_data(self._get_data(self._read_file()))
 
     def _read_file(self) -> dict:
         with open(self.file_path, encoding='utf-8') as file:
@@ -43,14 +45,25 @@ class ScheduleStorage:
     def get_data(self) -> dict:
         return self.data
 
+    def _convert_data(self, data: dict) -> dict:
+        for week_day in data.values():
+            for task in week_day:
+                task["begin_time"] = datetime.strptime(task["begin_time"], self.datetime_pattern)
+                task["end_time"] = datetime.strptime(task["end_time"], self.datetime_pattern)
+        return data
+
     def filter_by(self, week_day: str, date: str = None):
         result = self.data.get(week_day)
         if date:
-            result = filter(lambda task: task.get('begin_time').split(",")[0] == date, result)
+            date = datetime.strptime(date, "%d-%m-%Y")
+            result = filter(
+                lambda task: task.get('begin_time').date() == date.date(),
+                result
+            )
         return result
 
 
-schedule_storage = ScheduleStorage("storage.json", ("3_task", "schedule"))
+schedule_storage = ScheduleStorage("storage.json", ("3_task", "schedule"), "%d-%m-%Y, %H:%M:%S")
 
 
 @app.route("/schedule/<string:week_day>/<string:date>")
@@ -66,4 +79,4 @@ def get_concrete_day_schedule(week_day):
     return render_template("3_task/schedule.html", data=data, date=week_day)
 
 
-app.run(port=5000)
+app.run(port=5000, debug=True)
