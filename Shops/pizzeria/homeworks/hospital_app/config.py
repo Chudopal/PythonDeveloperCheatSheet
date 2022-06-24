6bf8e92063d7cab68e17f6aca26bc3005467d331
Eugene Syroezhkin <61776541+mega-yujin@users.noreply.github.com>
2022-06-24 14:25:16 +0300

    [#H18].(objects creation moved to config.py)

--- Shops/pizzeria/homeworks/hospital_app/config.py
+++ Shops/pizzeria/homeworks/hospital_app/config.py
@@ -1,2 +1,21 @@
 from flask import Flask
+from hospital import Hospital
+from storage_handler import DBStorage
+from query_builder import Table
+
+DB_CONNECTION_CONFIG = {
+    'host': "localhost",
+    'user': "admin",
+    'password': "admin",
+    'dbname': "homeworks"
+}
+
+storage = DBStorage(**DB_CONNECTION_CONFIG)
+
+doctors = Table('doctors', 'uuid', 'name', 'category', 'position')
+anamnesis = Table('anamnesis', 'anamnesis', 'patient_uuid', 'doctor_uuid', 'diagnosis', 'treatment')
+patients = Table('patients', 'uuid', 'name', 'birth_date', 'weight', 'height', 'sex')
+
+hospital = Hospital(storage=storage, patients=patients, doctors=doctors, anamnesis=anamnesis)
+
 app = Flask(__name__)
