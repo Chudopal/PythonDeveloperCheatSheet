1fc393093dffa18fd1316bdf22691364981549e1
Eugene Syroezhkin <mail.lpfun@gmail.com>
2022-07-05 22:17:51 +0300

    [#H21].(added admin view)

--- Shops/pizzeria/homeworks/django_shop_hw/shop/products/admin.py
+++ Shops/pizzeria/homeworks/django_shop_hw/shop/products/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from .models import Tag, Manufacturer, Product
+
 
 # Register your models here.
+class TagAdmin(admin.ModelAdmin):
+    model = Tag
+    list_display = ('name', 'quantity', 'discount')
+    ordering = ('name',)
+
+
+class ManufacturerAdmin(admin.ModelAdmin):
+    model = Manufacturer
+
+
+class ProductAdmin(admin.ModelAdmin):
+    model = Product
+    list_display = ('name', 'manufacturer', 'amount', 'price')
+
+
+admin.site.register(Tag, TagAdmin)
+admin.site.register(Manufacturer, ManufacturerAdmin)
+admin.site.register(Product, ProductAdmin)
