ffd7ac55993f738c33b9edb5d557e604bd0c0359
Eugene Syroezhkin <mail.lpfun@gmail.com>
2022-07-06 18:23:41 +0300

    [#H21].(implemented objects filtering by get params)

--- Shops/pizzeria/homeworks/django_shop_hw/shop/products/models.py
+++ Shops/pizzeria/homeworks/django_shop_hw/shop/products/models.py
@@ -5,6 +5,9 @@ from uuid import uuid4
 class Manufacturer(models.Model):
     name = models.CharField(max_length=255, null=False)
 
+    class Meta:
+        db_table = 'manufacturers'
+
     def __str__(self):
         return self.name
 
@@ -13,6 +16,9 @@ class Tag(models.Model):
     name = models.CharField(max_length=255, null=False)
     quantity = models.IntegerField(null=False)
     discount = models.IntegerField(null=False)
+
+    class Meta:
+        db_table = 'tags'
 
     def __str__(self):
         return self.name
@@ -26,6 +32,9 @@ class Product(models.Model):
     price = models.FloatField(null=False)
     manufacturer = models.ForeignKey(Manufacturer, on_delete=models.CASCADE)
     tags = models.ManyToManyField(Tag)
+
+    class Meta:
+        db_table = 'products'
 
     def __str__(self):
         return self.name
--- Shops/pizzeria/homeworks/django_shop_hw/shop/products/views.py
+++ Shops/pizzeria/homeworks/django_shop_hw/shop/products/views.py
@@ -1,47 +1,39 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render
 from .models import Manufacturer, Tag, Product
 
 
 # Create your views here.
 
-
-def price_lte(query_set, value):
-    return query_set.filter(price__lte=float(value))
-
-
-def price_gte(query_set, value):
-    return query_set.filter(price__gte=float(value))
+def format_args(request_args):
+    return {key: value.replace(' ', '').strip().split(",") for key, value in request_args.items()}
 
 
-def price_lt(query_set, value):
-    return query_set.filter(price__lt=float(value))
-
-
-def price_gt(query_set, value):
-    return query_set.filter(price__gt=float(value))
-
-
-price_filters = {
-    'price_lte': price_lte,
-    'price_gte': price_gte,
-    'price_lt': price_lt,
-    'price_gt': price_gt
+products_filters = {
+    'price_lte': lambda product, value: product.filter(price__lte=float(value)),
+    'price_gte': lambda product, value: product.filter(price__gte=float(value)),
+    'price_lt': lambda product, value: product.filter(price__lt=float(value)),
+    'price_gt': lambda product, value: product.filter(price__gt=float(value)),
+    'tags': lambda product, value: product.filter(tags__name=value)
 }
 
 
-def filter_products(**kwargs):
+def filter_products(request_get):
+    args = format_args(request_get)
     products = Product.objects.all()
-    if kwargs:
-        for arg, val in kwargs.items():
-            products = price_filters.get(arg)(products, val)
+    for arg, values in args.items():
+        for value in values:
+            query_filter = products_filters.get(arg, None)
+            if query_filter:
+                products = query_filter(products, value)
     return products
 
 
 def product_detail(request, product_uuid):
-    product = Product.objects.get(uuid=product_uuid)
-    return render(request=request, template_name='products/product_detail.html', context={'product': product})
+    product = Product.objects.select_related('manufacturer').prefetch_related('tags').get(uuid=product_uuid)
+    tags = product.tags.all()
+    return render(request=request, template_name='products/product_detail.html', context={'product': product, 'tags':tags})
 
 
 def products_view(request):
-    products = filter_products(**request.GET.dict())
+    products = filter_products(request.GET.dict())
     return render(request=request, template_name='products/products_list.html', context={'products': products})
