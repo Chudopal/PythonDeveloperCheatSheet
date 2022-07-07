2c7720dc87d174db2b44f1fac5615428acb96bd0
Eugene Syroezhkin <mail.lpfun@gmail.com>
2022-07-07 17:44:55 +0300

    [#H21].(added links to products in template)

--- Shops/pizzeria/homeworks/django_shop_hw/shop/products/urls.py
+++ Shops/pizzeria/homeworks/django_shop_hw/shop/products/urls.py
@@ -3,5 +3,5 @@ from .views import product_detail, products_view
 
 urlpatterns = [
     path('', products_view),
-    path('<str:product_uuid>/', product_detail),
+    path('<str:product_uuid>/', view=product_detail, name='product_detail'),
 ]
