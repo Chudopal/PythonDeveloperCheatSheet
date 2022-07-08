5cbf551afdb5f856e9f5bb81069bcaf656da548f
Eugene Syroezhkin <mail.lpfun@gmail.com>
2022-07-09 00:22:22 +0300

    [#H21].(added shop_service)

--- Shops/pizzeria/homeworks/django_shop_hw/shop/products/views.py
+++ Shops/pizzeria/homeworks/django_shop_hw/shop/products/views.py
@@ -2,35 +2,14 @@ from django.shortcuts import render
 from django.http import Http404
 from django.core.exceptions import ValidationError
 from .models import Product
+from .shop_service import ProductsService
 
-
-def format_args(request_args):
-    return {key: value.replace(' ', '').strip().split(",") for key, value in request_args.items()}
-
-
-products_filters = {
-    'price_lte': lambda product, value: product.filter(price__lte=float(value)),
-    'price_gte': lambda product, value: product.filter(price__gte=float(value)),
-    'price_lt': lambda product, value: product.filter(price__lt=float(value)),
-    'price_gt': lambda product, value: product.filter(price__gt=float(value)),
-    'tags': lambda product, value: product.filter(tags__name=value)
-}
-
-
-def filter_products(request_get):
-    args = format_args(request_get)
-    products = Product.objects.all()
-    for arg, values in args.items():
-        for value in values:
-            query_filter = products_filters.get(arg, None)
-            if query_filter:
-                products = query_filter(products, value)
-    return products
+products_service = ProductsService()
 
 
 def product_detail(request, product_uuid):
     try:
-        product = Product.objects.select_related('manufacturer').prefetch_related('tags').get(uuid=product_uuid)
+        product = products_service.get_product_by_uuid(product_uuid)
         tags = product.tags.all()
         return render(request=request, template_name='products/product_detail.html',
                       context={'product': product, 'tags': tags})
@@ -39,5 +18,5 @@ def product_detail(request, product_uuid):
 
 
 def products_view(request):
-    products = filter_products(request.GET.dict())
+    products = products_service.filter_products(request.GET.dict())
     return render(request=request, template_name='products/products_list.html', context={'products': products})
