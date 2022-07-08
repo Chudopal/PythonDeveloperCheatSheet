4c9f7f75d0744bea5147783a07f03f358ca1d648
Eugene Syroezhkin <61776541+mega-yujin@users.noreply.github.com>
2022-07-08 12:16:36 +0300

    [#H21].(added product saving)

--- Shops/pizzeria/homeworks/django_shop_hw/shop/products/admin_bot.py
+++ Shops/pizzeria/homeworks/django_shop_hw/shop/products/admin_bot.py
@@ -11,12 +11,12 @@ logger = telebot.logger
 telebot.logger.setLevel(logging.DEBUG)
 
 state_storage = StateMemoryStorage()
-callback_factory = CallbackData("item_pk", "storage_key", prefix='products')
+callback_factory = CallbackData("model_name", "item_pk", "user_id", "chat_id", prefix="products")
 
 
 def save_product(data):
     new_product = Product.objects.create(
-        name=data['product_name'],
+        name=data['name'],
         description=data['description'],
         amount=data['amount'],
         price=data['price'],
@@ -26,13 +26,12 @@ def save_product(data):
     new_product.save()
 
 
-def make_keyboard(queryset, storage_key):
+def make_keyboard(queryset, user_id, chat_id):
     keyboard = types.InlineKeyboardMarkup()
+    model_name = queryset.model.__name__
     for item in queryset:
-        keyboard.add(
-            types.InlineKeyboardButton(text=str(item),
-                                       callback_data=callback_factory.new(item_pk=item.pk, storage_key=storage_key))
-        )
+        callback_data = callback_factory.new(model_name=model_name, item_pk=item.pk, user_id=user_id, chat_id=chat_id)
+        keyboard.add(types.InlineKeyboardButton(text=str(item), callback_data=callback_data))
     return keyboard
 
 
@@ -49,16 +48,6 @@ class ProductAddStates(StatesGroup):
     amount = State()
     price = State()
     tags = State()
-
-
-class IsNotEmptyMessageFilter(SimpleCustomFilter):
-    """
-    Filter to check whether the string is empty.
-    """
-    key = 'is_not_null'
-
-    def check(self, message):
-        return message.text != ""
 
 
 class IsDigitFilter(SimpleCustomFilter):
@@ -81,7 +70,6 @@ bot = telebot.TeleBot(settings.BOT_TOKEN, state_storage=state_storage)
 bot.set_update_listener(listener)
 bot.add_custom_filter(custom_filters.StateFilter(bot))
 bot.add_custom_filter(IsDigitFilter())
-bot.add_custom_filter(IsNotEmptyMessageFilter())
 
 
 @bot.message_handler(commands=["start"])
@@ -91,7 +79,7 @@ def start(message):
 
 
 @bot.message_handler(commands=["add_product"])
-def add_product(message):
+def add_new_product(message):
     """
     Start of cycle. State is clear.
     """
@@ -109,16 +97,8 @@ def clear_state(message):
     bot.delete_state(message.from_user.id, message.chat.id)
 
 
-@bot.message_handler(state=ProductAddStates.product_name, is_not_null=False)
-def get_name_error(message):
-    """
-    State 1. Error handler. Will process when user sent an empty message at initial state.
-    """
-    bot.send_message(message.chat.id, 'Product name can`t be empty. Please try again.')
-
-
 @bot.message_handler(state=ProductAddStates.product_name)
-def get_name(message):
+def add_manufacturer(message):
     """
     State 1. Will process when user's state is ProductAddStates.product_name.
     """
@@ -126,22 +106,21 @@ def get_name(message):
         storage['name'] = message.text
 
     manufacturers = Manufacturer.objects.all()
-    keyboard = make_keyboard(queryset=manufacturers, storage_key='manufacturer')
+    keyboard = make_keyboard(queryset=manufacturers, chat_id=message.chat.id, user_id=message.from_user.id)
     bot.send_message(chat_id=message.chat.id, text='OK. Now choose a manufacturer', reply_markup=keyboard)
-    bot.set_state(message.from_user.id, ProductAddStates.manufacturer, message.chat.id)
+    bot.set_state(chat_id=message.chat.id, state=ProductAddStates.manufacturer, user_id=message.from_user.id)
 
 
-@bot.message_handler(state=ProductAddStates.manufacturer)
-def get_manufacturer(message):
+def add_description(chat_id, user_id):
     """
     State 2. Will process when user's state is ProductAddStates.manufacturer.
     """
-    bot.send_message(message.chat.id, 'Great! Now please provide a short description of your product (if you want)')
-    bot.set_state(message.from_user.id, ProductAddStates.description, message.chat.id)
+    bot.set_state(chat_id=chat_id, state=ProductAddStates.description, user_id=user_id)
+    bot.send_message(chat_id=chat_id, text='Great! Now please provide a short description of your product')
 
 
 @bot.message_handler(state=ProductAddStates.description)
-def get_description(message):
+def add_amount(message):
     """
     State 3. Will process when user's state is ProductAddStates.description.
     """
@@ -149,12 +128,11 @@ def get_description(message):
         storage['description'] = message.text
 
     bot.send_message(message.chat.id, 'Excellent! And how many products do you want to add?')
-    bot.send_message(message.chat.id, 'Just send an empty message if you want to leave default value (default = 1)')
     bot.set_state(message.from_user.id, ProductAddStates.amount, message.chat.id)
 
 
 @bot.message_handler(state=ProductAddStates.amount, is_digit=False)
-def get_amount_error(message):
+def add_amount_error(message):
     """
     State 4. Error handler. Will process when user entered a string.
     """
@@ -162,7 +140,7 @@ def get_amount_error(message):
 
 
 @bot.message_handler(state=ProductAddStates.amount)
-def get_amount(message):
+def add_price(message):
     """
     State 4. Will process when user's state is ProductAddStates.amount.
     """
@@ -174,16 +152,16 @@ def get_amount(message):
 
 
 @bot.message_handler(state=ProductAddStates.price)
-def get_price(message):
+def add_tags(message):
     """
     State 5. Will process when user's state is ProductAddStates.price.
     """
-    if IsDigitFilter().check(message) and IsNotEmptyMessageFilter().check(message):
+    if IsDigitFilter().check(message):
         with bot.retrieve_data(message.from_user.id, message.chat.id) as storage:
             storage['price'] = message.text
 
         tags = Tag.objects.all()
-        keyboard = make_keyboard(storage_key='tags', queryset=tags)
+        keyboard = make_keyboard(queryset=tags, user_id=message.from_user.id, chat_id=message.chat.id)
         bot.send_message(message.chat.id, 'And finally please select tags for your product', reply_markup=keyboard)
         bot.set_state(message.from_user.id, ProductAddStates.tags, message.chat.id)
 
@@ -192,15 +170,21 @@ def get_price(message):
         return
 
 
-@bot.message_handler(state=ProductAddStates.tags)
-def get_tags(message):
+def add_another_tag(chat_id, user_id):
+    tags = Tag.objects.all()
+    text = 'Wanna add another one tag? Or type "/enough_tags" to go to the next step.'
+    keyboard = make_keyboard(queryset=tags, user_id=user_id, chat_id=chat_id)
+    bot.send_message(chat_id=chat_id, text=text, reply_markup=keyboard)
+
+
+@bot.message_handler(state=ProductAddStates.tags, commands=["enough_tags"])
+def show_result(message):
     """
     State 6. Will process when user's state is ProductAddStates.tags.
     Final state of cycle
     """
     result = str()
-    with bot.retrieve_data(message.from_user.id, message.chat.id) as storage:
-        storage['tags'] = message.text
+    with bot.retrieve_data(user_id=message.chat.id, chat_id=message.chat.id) as storage:
         result += (f'Your product:\n<b>'
                    f'Product: {storage["name"]}\n'
                    f'Manufacturer: {storage["manufacturer"]}\n'
@@ -209,22 +193,47 @@ def get_tags(message):
                    f'Price: {storage["price"]}\n'
                    f'Tags: {storage["tags"]}</b>')
 
-    bot.send_message(message.chat.id, 'Ready! Your product has been added!')
-    bot.send_message(message.chat.id, result, parse_mode="html")
-    bot.delete_state(message.from_user.id, message.chat.id)
+        save_product(storage)
+    bot.send_message(chat_id=message.chat.id, text='Ready! Your product has been added!')
+    bot.send_message(chat_id=message.chat.id, text=result, parse_mode="html")
+    bot.delete_state(user_id=message.chat.id, chat_id=message.chat.id)
+
+
+def process_tags(data: dict):
+    chat_id = data.get('chat_id')
+    user_id = data.get('user_id')
+    item_pk = data.get('item_pk')
+
+    tag = Tag.objects.get(pk=item_pk)
+
+    with bot.retrieve_data(user_id=user_id, chat_id=chat_id) as storage:
+        tags = storage.get('tags', [])
+        storage['tags'] = tags.append(tag)
+
+    add_another_tag(chat_id=chat_id, user_id=user_id)
+
+
+def process_manufacturer(data: dict):
+    chat_id = data.get('chat_id')
+    user_id = data.get('user_id')
+    item_pk = data.get('item_pk')
+
+    manufacturer = Manufacturer.objects.get(pk=item_pk)
+
+    with bot.retrieve_data(user_id=user_id, chat_id=chat_id) as storage:
+        storage['manufacturer'] = manufacturer
+
+    add_description(chat_id=chat_id, user_id=user_id)
 
 
 @bot.callback_query_handler(func=lambda call: True)
 def callback_handler(call):
     models = {
-        'tags': Tag.objects.get,
-        'manufacturer': Manufacturer.objects.get
+        'Manufacturer': process_manufacturer,
+        'Tag': process_tags
     }
 
-    callback_data: dict = callback_factory.parse(callback_data=call.data)
-    storage_key = callback_data.get('storage_key')
-    item_pk = callback_data.get('item_pk')
-
-    data = models.get(storage_key)(pk=item_pk)
-    with bot.retrieve_data(user_id=call.message.chat.id, chat_id=call.message.chat.id) as storage:
-        storage[storage_key] = data
+    callback_data = callback_factory.parse(callback_data=call.data)
+    model_name = callback_data.get('model_name')
+    models.get(model_name)(callback_data)
+    bot.answer_callback_query(call.id)
