c9568e89f1d7568fb76123fc9ddbb27ec7be7943
Xejb <vladiq-artamonenko@rambler.ru>
2022-07-24 17:16:20 +0300

    [#D04] added domain layer, added data models

--- Shops/hardware_store/app/main.py
+++ Shops/hardware_store/app/main.py
@@ -1,7 +1,21 @@
 from typing import List, Dict
 from abc import ABC, abstractmethod
+from dataclasses import dataclass
 from psycopg2 import sql
 import psycopg2
+
+
+@dataclass
+class ShopInvent:
+    id: str
+    name: str
+    price: float
+
+
+@dataclass
+class ShoppingCart:
+    id: str
+    price: float
 
 
 class StorageHandler(ABC):
@@ -51,11 +65,15 @@ class DataBaseHandler(StorageHandler):
         self._password = password
         self._host = host
         self._table_name = table_name
+        self._connector = psycopg2.connect(host=self._host, user=self._user,
+                                           password=self._password, dbname=self._dbname)
 
-    def get_data(self) -> Dict:
+    def get_data(self) -> list[dict]:
         sql_query = self._select_query()
         db_response = self.execute(sql_query)
-        return db_response.fetchall()
+        columns = [item[0] for item in db_response.description]
+        data = [dict(zip(columns, row)) for row in db_response.fetchall()]
+        return data
 
     def add_item(self, item: dict):
         sql_query = self._insert_query(item)
@@ -66,10 +84,9 @@ class DataBaseHandler(StorageHandler):
         self.execute(sql_query)
 
     def execute(self, sql_query):
-        connector = psycopg2.connect(host=self._host, user=self._user, password=self._password, dbname=self._dbname)
-        cursor = connector.cursor()
+        cursor = self._connector.cursor()
         cursor.execute(sql_query)
-        connector.commit()
+        self._connector.commit()
         return cursor
 
     def _select_query(self) -> psycopg2.sql:
@@ -82,7 +99,7 @@ class DataBaseHandler(StorageHandler):
         query = sql.SQL("INSERT INTO {table} ({columns}) VALUES ({values})").format(
             table=sql.SQL(self._table_name),
             columns=sql.SQL(", ").join([sql.Identifier(col) for col in variables.keys()]),
-            values=sql.SQL(", ").join([sql.Literal(val) for val in variables.values()])
+            values=sql.SQL(", ").join([sql.Literal(val) for val in variables.keys()])
         )
         query += sql.SQL(";")
         return query
@@ -96,7 +113,7 @@ class DataBaseHandler(StorageHandler):
 
 
 class ShopService:
-    SHOP = {}
+    SHOP = []
 
     def __init__(self, dbname: str, user: str,
                  password: str, host: str,
@@ -106,34 +123,33 @@ class ShopService:
         self._shopping_card_storage = Storage(DataBaseHandler(dbname, user,
                                                               password, host, shopping_card_table_name))
 
-    def get_all_product(self) -> Dict:
+    def get_all_product(self) -> List[ShopInvent]:
         if not self.SHOP:
-            all_product = self._shop_invent_storage.get_data()
-            all_product_dict = {x[1]: x for x in all_product}
-            self.SHOP.update(all_product_dict)
+            all_product = [ShopInvent(**product) for product in self._shop_invent_storage.get_data()]
+            self.SHOP = all_product
         return self.SHOP
 
     def add_product(self, product_name: str) -> None:
-        price = self.get_all_product().get(product_name)[2]
-        self._shopping_card_storage.add_item({'price': price})
+        product = next((item for item in self.get_all_product() if item.name == product_name), None)
+        self._shopping_card_storage.add_item({'price': product.price})
 
-    def get_buy_product(self) -> List:
-        shopping_cart = self._shopping_card_storage.get_data()
-        prices = list(map(lambda x: x[1], shopping_cart))
+    def get_buy_product(self) -> list[dict]:
+        shopping_cart = [ShoppingCart(**shopping_cart) for shopping_cart in self._shopping_card_storage.get_data()]
+        prices = list(map(lambda x: x.price, shopping_cart))
         result = prices
         return result
 
-    def format_product(self, product_list: Dict) -> str:
+    def format_product(self, product_list: list[ShopInvent]) -> str:
         return '\n'.join([
-            f'{product_name} - {product[2]} dollars'
-            for product_name, product in product_list.items()
+            f'{product.name} - {product.price} dollars'
+            for product in product_list
         ])
 
     def clear_buy_products(self):
         self._shopping_card_storage.remove_all()
 
-    def check_product(self, product_name):
-        return self.get_all_product().get(product_name)
+    def check_product(self, product_name) -> ShopInvent:
+        return next((item for item in self.get_all_product() if item.name == product_name), None)
 
 
 class Shop:
