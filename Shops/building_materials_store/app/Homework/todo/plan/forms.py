172cc42fdba7fdd420abe302edab2080f5b2b6c5
Nikita5390 <nikitakashevskii@mail.ru>
2022-07-27 21:23:44 +0300

    [#H22].(added 4ex. in medium)

---
+++ Shops/building_materials_store/app/Homework/todo/plan/forms.py
@@ -0,0 +1,58 @@
+from datetime import date, timedelta
+
+from django import forms
+
+from .models import Event
+
+
+class EventForm(forms.ModelForm):
+    class Meta:
+        model = Event
+        fields = [
+            'started_at', 'finished_at', 'title',
+            'description', 'depends_on', 'status',
+        ]
+        widgets = {
+            'title': forms.TextInput(attrs={
+                'class': 'form-control',
+            }),
+            'description': forms.Textarea(attrs={
+                'class': 'form-control',
+            }),
+            'finished_at': forms.DateInput(attrs={
+                'type': 'date',
+                'class': 'form-control',
+            }),
+            'started_at': forms.DateInput(attrs={
+                'type': 'date',
+                'class': 'form-control',
+            }),
+            'status': forms.Select(attrs={
+                'class': 'form-select',
+            }),
+            'depends_on': forms.Select(attrs={
+                'class': 'form-select',
+            })
+        }
+
+    def clean_started_at(self):
+        started_at = self.cleaned_data['started_at']
+
+        if started_at < date.today():
+            raise forms.ValidationError("Вы не можете создать событие, которое начась в прошлом!")
+
+        return started_at
+
+    def clean_finished_at(self):
+        started_at = self.cleaned_data['started_at']
+        finished_at = self.cleaned_data['finished_at']
+        info = finished_at > started_at
+        dead_line = finished_at - started_at
+
+        if info == False and dead_line != timedelta(days=0):
+            raise forms.ValidationError("Вы не можете создать событие, которое заканчивается раньше своего начала!")
+
+        if dead_line > timedelta(days=14):
+            raise forms.ValidationError("Вы не можете создать событие с дедлайном больше 14 дней!")
+
+        return finished_at
