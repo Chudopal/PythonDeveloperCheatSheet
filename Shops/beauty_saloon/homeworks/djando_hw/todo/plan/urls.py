0565d9790212073bb2e334078e6bca33380f837a
Sergey <Skromnik02@gmail.com>
2022-07-28 15:21:39 +0300

    [#H22].(adding an easy level)

---
+++ Shops/beauty_saloon/homeworks/djando_hw/todo/plan/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path
+from .views import (
+    EventCreateView,
+    EventListView,
+    EventDetailView,
+    EventDeleteView,
+    EventUpdateView,
+)
+
+urlpatterns = [
+    path("", view=EventCreateView.as_view(), name="event-add"),
+    path("add/", view=EventCreateView.as_view(), name="event-add"),
+    path("list/", view=EventListView.as_view(), name="event-list"),
+    path("<str:pk>/", view=EventDetailView.as_view(), name="event-detail"),
+    path("<str:pk>/delete", view=EventDeleteView.as_view(), name="event-delete"),
+    path("<str:pk>/update", view=EventUpdateView.as_view(), name="event-update"),
+]
