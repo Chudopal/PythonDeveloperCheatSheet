4ef18bbe27e02e7f6c4af32c5181b596ebeb2dfd
Uladzislau Shedko(Foranx team) <v.shedko@foranx.by>
2022-08-02 11:28:25 +0300

    [H20].(FIX, deleted sql file)

--- Shops/household_shop/homeworks/django_hw/structure.sql
+++
@@ -1,8 +0,0 @@
--- Сделайте пользователя manager в PostgreSQL следующей командой:
-CREATE USER manager WITH ENCRYPTED PASSWORD 'manager';
-
--- Дайте новому пользователю права в новой базе данных командой:
-GRANT ALL PRIVILEGES ON DATABASE bank_system TO manager;
-
--- PORT может оказаться у вас другим, его можно посмотреть в БД командой:
-SELECT setting FROM pg_settings WHERE name = 'port';
