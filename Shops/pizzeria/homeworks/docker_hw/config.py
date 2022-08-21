4a4e7fe589eeacbf6ab19730133e6c0794c23553
Eugene Syroezhkin <mail.lpfun@gmail.com>
2022-08-21 17:28:41 +0300

    [#H24].(implemented json config file)

--- Shops/pizzeria/homeworks/docker_hw/config.py
+++ Shops/pizzeria/homeworks/docker_hw/config.py
@@ -1,4 +1,4 @@
-import os
+import json
 from dataclasses import dataclass
 from flask import Flask
 from peewee import SqliteDatabase
@@ -6,12 +6,15 @@ from peewee import SqliteDatabase
 
 @dataclass
 class BaseConfig:
-    SERVER_NAME = '127.0.0.1:5000'
-    DEBUG = True
-    SECRET_KEY = os.environ.get('os_secret_key')
-    DB_NAME = 'sqlite.db'
+    SERVER_NAME: str
+    DEBUG: bool
+    SECRET_KEY: str
+    DB_NAME: str
 
+
+with open('config.json') as json_config:
+    base_config = BaseConfig(**json.load(json_config))
 
 app = Flask(__name__)
-app.config.from_object(BaseConfig)
+app.config.from_object(base_config)
 db = SqliteDatabase(app.config.get('DB_NAME'))
