e94e4521e25ea75f9809a72d35d196f1de1b42f7
Eugene Syroezhkin <mail.lpfun@gmail.com>
2022-08-28 15:30:04 +0300

    [#H25].(added "id" field to serializer class)

--- Shops/pizzeria/homeworks/django_todo/plan_api/serializers.py
+++ Shops/pizzeria/homeworks/django_todo/plan_api/serializers.py
@@ -5,4 +5,4 @@ from plan.models import Event
 class EventSerializer(serializers.ModelSerializer):
     class Meta:
         model = Event
-        fields = ['created_at', 'started_at', 'finished_at', 'title', 'description', 'depends_on', 'status']
+        fields = ['id', 'created_at', 'started_at', 'finished_at', 'title', 'description', 'depends_on', 'status']
